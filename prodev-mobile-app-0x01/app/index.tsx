c50be1175a86ed3b1d0afa523ef10d2bfd83ac13
jungles001 <olusojiisraelife@gmail.com>
2025-10-29 20:09:51 +0100

    Implementing Mobile Components in React Native

--- prodev-mobile-app-0x01/app/index.tsx
+++ prodev-mobile-app-0x01/app/index.tsx
@@ -3,7 +3,7 @@ import { Text, View, StyleSheet } from "react-native";
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text>Enter Screen - Awesome</Text>
+      <Text>Entry Screen - Awesome</Text>
       <View>
         <Text style={styles.largeText}>
           Typescript is great if you practice more
